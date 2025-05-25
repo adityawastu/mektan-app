a8d81cc68f78e723ad8858a9b1e6454df617e406
aditya wastu <adityawastu37@gmail.com>
2025-05-25 15:49:15 +0700

    perubahan sidebar menu

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -120,18 +120,33 @@
                             <li>
                                 <a href="#"
                                     class="flex items-center p-2 pl-11 w-full text-base font-sm text-gray-900 rounded-lg transition duration-75 group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700">Data
-                                    Asset Alsintan</a>
+                                    Alsintan</a>
                             </li>
                             <li>
                                 <a href="#"
-                                    class="flex items-center p-2 pl-11 w-full text-base font-sm text-gray-900 rounded-lg transition duration-75 group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700">Pemeliharaan
-                                    Asset</a>
+                                    class="flex items-center p-2 pl-11 w-full text-base font-sm text-gray-900 rounded-lg transition duration-75 group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700">Peta
+                                    & Lokasi</a>
                             </li>
                             <li>
                                 <a href="#"
-                                    class="flex items-center p-2 pl-11 w-full text-base font-sm  text-gray-900 rounded-lg transition duration-75 group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700">Calendar</a>
+                                    class="flex items-center p-2 pl-11 w-full text-base font-sm  text-gray-900 rounded-lg transition duration-75 group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700">Monitoring
+                                    Aktivitas</a>
                             </li>
                         </ul>
+                    </li>
+                    <li>
+                        <a href="#"
+                            class="flex items-center p-2 text-base font-normal text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
+
+                            <svg class="w-6 h-6 text-gray-800 dark:text-white" aria-hidden="true"
+                                xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none"
+                                viewBox="0 0 24 24">
+                                <path stroke="currentColor" stroke-linecap="round" stroke-width="2"
+                                    d="M4.5 17H4a1 1 0 0 1-1-1 3 3 0 0 1 3-3h1m0-3.05A2.5 2.5 0 1 1 9 5.5M19.5 17h.5a1 1 0 0 0 1-1 3 3 0 0 0-3-3h-1m0-3.05a2.5 2.5 0 1 0-2-4.45m.5 13.5h-7a1 1 0 0 1-1-1 3 3 0 0 1 3-3h3a3 3 0 0 1 3 3 1 1 0 0 1-1 1Zm-1-9.5a2.5 2.5 0 1 1-5 0 2.5 2.5 0 0 1 5 0Z" />
+                            </svg>
+
+                            <span class="ml-2">Manajemen Pengguna</span>
+                        </a>
                     </li>
                 </ul>
             </div>
