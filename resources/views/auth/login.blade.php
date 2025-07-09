9fc89d0725bbcbc06fad0207ae10441e17faffdb
aditya wastu <adityawastu37@gmail.com>
2025-07-09 15:23:31 +0700

    tampilan

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,4 +1,18 @@
-<section class="bg-gray-50 dark:bg-gray-900">
+
+
+<!-- resources/views/welcome.blade.php -->
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+   <meta charset="UTF-8">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   @vite(['resources/css/app.css', 'resources/js/app.js'])
+   <title>Mektan-App</title>
+</head>
+
+<body>
+    <section class="bg-gray-50 dark:bg-gray-900">
   <div class="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
       <a href="#" class="flex items-center mb-6 text-2xl font-semibold text-gray-900 dark:text-white">
           <img class="w-8 h-8 mr-2" src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/logo.svg" alt="logo">
@@ -38,3 +52,5 @@
       </div>
   </div>
 </section>
+</body>
+</html>
